873c25fd27b22dfb43cb79a6b8a2708199a34191
Saliou Akande <lawalsaliouakande@gmail.com>
2024-03-09 15:47:22 +0100

    initial commit

--- src/Views/Pages.jsx
+++ src/Views/Pages.jsx
@@ -4,15 +4,48 @@ import Home from "./Home";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import ErrorPage from "./ErrorPage";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Pages = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/singup" element={<Signup />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
+    <HelmetProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Helmet>
+                <title>Improve customer service today | Ayuda</title>
+              </Helmet>
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <>
+              <Helmet>
+                <title>Sign in | Ayuda</title>
+              </Helmet>
+              <Signin />
+            </>
+          }
+        />
+        <Route
+          path="/singup"
+          element={
+            <>
+              <Helmet>
+                <title>Sign up | Ayuda</title>
+              </Helmet>
+              <Signup />
+            </>
+          }
+        />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </HelmetProvider>
   );
 };
 
